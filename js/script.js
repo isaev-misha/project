b944f0a11fe316d7f63e7ab2c74c8c4b0c3288a7
Mikhail <example@gmail.com>
2022-01-11 18:20:25 +0300

    027 Tasks

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-            while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-                personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-        }       
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
     },
-    rememberMyFilms: function() {
+    rememberMyFilms: function () {
         for (let i = 0; i < 2; i++) {
             const a = prompt('Один из последних просмотренных фильмов?', ''),
-                  b = prompt('На сколько оцените его?', '');
-        
+                b = prompt('На сколько оцените его?', '');
+
             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
                 personalMovieDB.movies[a] = b;
                 console.log('done');
@@ -26,7 +26,7 @@ const personalMovieDB = {
             }
         }
     },
-    detectPersonalLevel: function() {
+    detectPersonalLevel: function () {
         if (personalMovieDB.count < 10) {
             console.log("Просмотрено довольно мало фильмов");
         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -37,31 +37,33 @@ const personalMovieDB = {
             console.log("Произошла ошибка");
         }
     },
-    sjowMyDB: function(hidden) {
+    sjowMyDB: function (hidden) {
         if (!hidden) {
             console.log("выводим объект");
             console.log(personalMovieDB);
         }
     },
-    writeYourGenres: function() {
-        for (let i=1; i< 4; i++) {
-            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`,'');
-            while(personalMovieDB.genres[i - 1] == '' || personalMovieDB.genres[i - 1] == null || isNaN(personalMovieDB.genres[i - 1])) {
-                personalMovieDB.genres[i - 1]  = prompt(`Ваш любимый жанр под номером ${i}`,'');
-            }                 
+    writeYourGenres: function () {
+        for (let i = 1; i < 4; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (genre === '' || genre === null) {
+                console.log(`Вы ввели некорректные данные`);
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+
+            personalMovieDB.genres.forEach((item, i) => {
+                console.log(`Любимый жанр  ${i + 1} - это ${item}`);
+            });
         }
-        personalMovieDB.genres.forEach(function(item, y, arr) {
-            console.log(`Любимый жанр # ${y} - это ${arr}`);
-        });
-        //console.log("genres = "+personalMovieDB.genres);
     },
-    toggleVisiblemyBD: function() {
+    toggleVisiblemyBD: function () {
         if (personalMovieDB.privat) {
             personalMovieDB.privat = false;
         } else {
             personalMovieDB.privat = true;
         }
     }
-};
+}
 
-//personalMovieDB.writeYourGenres();
+personalMovieDB.writeYourGenres();
---
+++ js/script027.js
@@ -0,0 +1,4 @@
+"use sctrict";
+alert(typeof(1+"Infinity" ));
+console.log(1+"Infinity");
+console.log(typeof(1+"Infinity" ));
