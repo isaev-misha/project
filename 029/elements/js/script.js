642e8aef2accb8147cbea555bb5a8d851f35dea4
Mikhail <example@gmail.com>
2022-10-22 12:52:52 +0300

    029 Tasks

--- 029/elements/js/script.js
+++ 029/elements/js/script.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const box = document.getElementById('box'); 
-console.log(box);
-
 const btns = document.getElementsByTagName('button');
-console.log(btns[0]);
-const circle = document.getElementsByClassName('circle')
-console.log(circle);
-
+const circles = document.getElementsByClassName('circle')
 const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-hearts.forEach( item => {
-    console.log(hearts);
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
 });
 
-const heart = document.querySelector('.heart');
-console.log(heart);
+const div = document.createElement('div');
+
+div.classList.add('black');
+//document.body.append(div);
+wrapper.append(div);
+//hearts.after(div);
+div.innerHTML = '<h1>hello world</h1>';
